9b82bce9136394b598424fba6c71576f88b40597
Wendy <wryao64@gmail.com>
2018-11-19 17:07:14 +1300

    created blob storage to upload images

--- BookDatabaseAPI/Controllers/BookController.cs
+++ BookDatabaseAPI/Controllers/BookController.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BookDatabaseAPI.Models;
+using BookDatabaseAPI.Helpers;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
 
 namespace BookDatabaseAPI.Controllers
 {
@@ -14,10 +19,12 @@ namespace BookDatabaseAPI.Controllers
     public class BookController : ControllerBase
     {
         private readonly BookDatabaseAPIContext _context;
+        private IConfiguration _configuration;
 
-        public BookController(BookDatabaseAPIContext context)
+        public BookController(BookDatabaseAPIContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         // GET: api/Book
@@ -127,12 +134,116 @@ namespace BookDatabaseAPI.Controllers
         [HttpGet]
         public async Task<List<string>> GetTags()
         {
-            var memes = (from m in _context.BookItem
-                         select m.Tags).Distinct();
+            var books = (from b in _context.BookItem
+                         select b.Tags).Distinct();
 
-            var returned = await memes.ToListAsync();
+            var returned = await books.ToListAsync();
 
             return returned;
+        }
+
+        // POST: api/Book/upload
+        [HttpPost, Route("upload")]
+        public async Task<IActionResult> UploadFile([FromForm]BookImageItem book)
+        {
+            if (!MultipartRequestHelper.IsMultipartContentType(Request.ContentType))
+            {
+                return BadRequest($"Expected a multipart request, but got {Request.ContentType}");
+            }
+            try
+            {
+                using (var stream = book.Image.OpenReadStream())
+                {
+                    var cloudBlock = await UploadToBlob(book.Image.FileName, null, stream);
+                    //// Retrieve the filename of the file you have uploaded
+                    //var filename = provider.FileData.FirstOrDefault()?.LocalFileName;
+                    if (string.IsNullOrEmpty(cloudBlock.StorageUri.ToString()))
+                    {
+                        return BadRequest("An error has occured while uploading your file. Please try again.");
+                    }
+
+                    BookItem bookItem = new BookItem();
+                    bookItem.Title = book.Title;
+                    bookItem.Author = book.Author;
+                    bookItem.Synopsis = book.Synopsis;
+                    bookItem.Tags = book.Tags;
+
+                    System.Drawing.Image image = System.Drawing.Image.FromStream(stream);
+                    bookItem.Height = image.Height.ToString();
+                    bookItem.Width = image.Width.ToString();
+                    bookItem.Url = cloudBlock.SnapshotQualifiedUri.AbsoluteUri;
+                    bookItem.Uploaded = DateTime.Now.ToString();
+
+                    _context.BookItem.Add(bookItem);
+                    await _context.SaveChangesAsync();
+
+                    return Ok($"File: {book.Title} has successfully uploaded");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error has occured. Details: {ex.Message}");
+            }
+
+
+        }
+
+        private async Task<CloudBlockBlob> UploadToBlob(string filename, byte[] imageBuffer = null, System.IO.Stream stream = null)
+        {
+
+            var accountName = _configuration["AzureBlob:name"];
+            var accountKey = _configuration["AzureBlob:key"]; ;
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer imagesContainer = blobClient.GetContainerReference("images");
+
+            string storageConnectionString = _configuration["AzureBlob:connectionString"];
+
+            // Check whether the connection string can be parsed.
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                try
+                {
+                    // Generate a new filename for every new blob
+                    var fileName = Guid.NewGuid().ToString();
+                    fileName += GetFileExtention(filename);
+
+                    // Get a reference to the blob address, then upload the file to the blob.
+                    CloudBlockBlob cloudBlockBlob = imagesContainer.GetBlockBlobReference(fileName);
+
+                    if (stream != null)
+                    {
+                        await cloudBlockBlob.UploadFromStreamAsync(stream);
+                    }
+                    else
+                    {
+                        return new CloudBlockBlob(new Uri(""));
+                    }
+
+                    return cloudBlockBlob;
+                }
+                catch (StorageException ex)
+                {
+                    return new CloudBlockBlob(new Uri(""));
+                }
+            }
+            else
+            {
+                return new CloudBlockBlob(new Uri(""));
+            }
+
+        }
+
+        private string GetFileExtention(string fileName)
+        {
+            if (!fileName.Contains("."))
+                return ""; //no extension
+            else
+            {
+                var extentionList = fileName.Split('.');
+                return "." + extentionList.Last(); //assumes last item is the extension 
+            }
         }
     }
 }
---
+++ BookDatabaseAPI/Models/BookImageItem.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookDatabaseAPI.Models
+{
+    public class BookImageItem
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Synopsis { get; set; }
+        public string Tags { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
