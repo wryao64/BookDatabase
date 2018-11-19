a80e3dc75c2adb33b7ad9c4dee75b488fcc5aa31
Wendy <wryao64@gmail.com>
2018-11-19 14:42:08 +1300

    created base project for API

---
+++ BookDatabaseAPI/Data/BookDatabaseAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookDatabaseAPI.Models
+{
+    public class BookDatabaseAPIContext : DbContext
+    {
+        public BookDatabaseAPIContext (DbContextOptions<BookDatabaseAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookDatabaseAPI.Models.BookItem> BookItem { get; set; }
+    }
+}
---
+++ BookDatabaseAPI/Models/BookItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookDatabaseAPI.Models
+{
+    public class BookItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Synopsis { get; set; }
+        public string Url { get; set; }
+        public string Tags { get; set; }
+        public string Uploaded { get; set; }
+        public string Width { get; set; }
+        public string Height { get; set; }
+    }
+}
