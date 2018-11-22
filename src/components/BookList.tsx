c303c066c2a6c8432b03fa6071009a45b267307e
Wendy <wryao64@gmail.com>
2018-11-22 17:28:18 +1300

    speech-to-text works

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import MediaStreamRecorder from 'msr';
 
 interface IProps {
     books: any[],
@@ -10,6 +11,8 @@ export default class BookList extends React.Component<IProps, {}> {
     constructor(props: any) {
         super(props)   
         this.searchByTag = this.searchByTag.bind(this)
+        this.postAudio = this.postAudio.bind(this)
+        this.searchTagByVoice = this.searchTagByVoice.bind(this)
     }
 
 	public render() {
@@ -21,6 +24,7 @@ export default class BookList extends React.Component<IProps, {}> {
                         <div className="input-group-append">
                             <div className="btn btn-outline-secondary search-button" onClick = {this.searchByTag}>Search</div>
                         </div>
+                        <div className="btn" onClick={this.searchTagByVoice}><i className="fa fa-microphone" /></div>
                     </div>  
                 </div>
                 <div className="row book-list-table">
@@ -71,4 +75,68 @@ export default class BookList extends React.Component<IProps, {}> {
         this.props.searchByTag(tag)  
     }
 
+    private searchTagByVoice() {
+        const mediaConstraints = {
+            audio: true
+        }
+        const env = this;
+        function onMediaSuccess(stream: any) {
+            const mediaRecorder = new MediaStreamRecorder(stream);
+            mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
+            mediaRecorder.ondataavailable = (blob:any) => {
+                env.postAudio(blob);
+                mediaRecorder.stop()
+            }
+            mediaRecorder.start(3000);
+        }
+ 
+        function onMediaError(e: any) {
+            console.error('media error', e);
+        }
+ 
+        navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError)
+    }
+
+    private postAudio(blob: any) {
+        let accessToken: any;
+        fetch('https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken', {
+            headers: {
+                'Content-Length': '0',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Ocp-Apim-Subscription-Key': 'ac4a5739b2fa4d749b080822e01c167a'
+            },
+            method: 'POST'
+        }).then((response) => {
+            console.log("------------RESPONSE1----------------")
+            console.log(response.text())
+            console.log("------------RESPONSE1 end----------------")
+            return response
+        }).then((response) => {
+            console.log("------------RESPONSE2----------------")
+            console.log(response)
+            accessToken = response
+        }).catch((error) => {
+            console.log("Error", error)
+        });
+
+        // posting audio
+        fetch('https://westus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US', {
+            body: blob, // this is a .wav audio file    
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer' + accessToken,
+                'Content-Type': 'audio/wav;codec=audio/pcm; samplerate=16000',
+                'Ocp-Apim-Subscription-Key': 'ac4a5739b2fa4d749b080822e01c167a'
+            },    
+            method: 'POST'
+        }).then((res) => {
+            return res.json()
+        }).then((res: any) => {
+            console.log(res)
+            const textBox = document.getElementById("search-tag-textbox") as HTMLInputElement
+            textBox.value = (res.DisplayText as string).slice(0, -1)
+        }).catch((error) => {
+            console.log("Error", error)
+        });
+    }
 }
